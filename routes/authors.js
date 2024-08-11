e1e89f4cd13009aaf0386335b4b9a24ab82a931c
saadhiq <hassaan.edu@gmail.com>
2024-08-11 16:46:38 +0530

    all second tuto

--- routes/authors.js
+++ routes/authors.js
@@ -5,8 +5,22 @@ const Author = require('../models/author')
 
 
 // All Authers Routs
-router.get('/', (req,res) => {
-    res.render('authors/index')
+router.get('/', async (req,res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors, 
+            searchOptions: req.query
+        })
+
+    }
+    catch{
+        res.redirect('/')
+    }
 })
 
 // New Auther Route
